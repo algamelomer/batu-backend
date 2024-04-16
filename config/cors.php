9bf3dfa1ea8586947ff18994e05eb56366d6db58
Ahmedsultan2021 <ahmedsultan4277892@gmail.com>
2024-04-16 21:02:45 +0200

    fix bugs

--- config/cors.php
+++ config/cors.php
@@ -1,33 +1,18 @@
 <?php
 
+// config/cors.php
+
+// config/cors.php
+
 return [
-
-    /*
-    |--------------------------------------------------------------------------
-    | Cross-Origin Resource Sharing (CORS) Configuration
-    |--------------------------------------------------------------------------
-    |
-    | Here you may configure your settings for cross-origin resource sharing
-    | or "CORS". This determines what cross-origin operations may execute
-    | in web browsers. You are free to adjust these settings as needed.
-    |
-    | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
-    |
-    */
-
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
-
-    'allowed_methods' => ['*'],
-
-    'allowed_origins' => [env('env', 'https://batechu.com')],
-
-    'allowed_origins_patterns' => [],
-
+    'paths' => ['api/*'],
+    'allowed_methods' => ['GET', 'POST', 'PUT', 'DELETE'],
+    'allowed_origins' => [
+        'http://localhost:5173', // Development frontend URL
+        'https://batechu.com'    // Production frontend URL
+    ],
     'allowed_headers' => ['*'],
-
     'exposed_headers' => [],
-
     'max_age' => 0,
-
     'supports_credentials' => true,
 ];
