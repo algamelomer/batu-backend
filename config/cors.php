709fb80502db302677d17297ab1b0780edcfb4ce
Ahmedsultan2021 <ahmedsultan4277892@gmail.com>
2024-04-17 05:36:41 +0200

    fix bugs

--- config/cors.php
+++ config/cors.php
@@ -9,9 +9,20 @@ return [
     'allowed_methods' => ['GET', 'POST', 'PUT', 'DELETE'],
     'allowed_origins' => [
         'http://localhost:5173', // Development frontend URL
-        'https://batechu.com'    // Production frontend URL
+        'http://127.0.0.1:5173', // Development frontend URL
+        'https://127.0.0.1:5173', // Development frontend URL
+        'https://batechu.com',   // Production frontend URL
+
+        'http://127.0.0.1:8000', // Development backend URL
+        'https://127.0.0.1:8000', // Development backend URL
+        'https://admin.batechu.com/', // production backend URL
+
+        
+
+
     ],
     'allowed_headers' => ['*'],
+    'allowed_origins_patterns' => [],
     'exposed_headers' => [],
     'max_age' => 0,
     'supports_credentials' => true,
